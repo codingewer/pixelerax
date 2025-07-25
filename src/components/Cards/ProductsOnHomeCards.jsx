d250b013bf80b40deffff87582eec7ffb3641681
codingewer <yatli5645@gmail.com>
2025-07-25 23:33:38 +0300

    update

--- src/components/Cards/ProductsOnHomeCards.jsx
+++ src/components/Cards/ProductsOnHomeCards.jsx
@@ -30,29 +30,31 @@ function ProductsOnHomeCards() {
                     <svg className={styles.borderAnim} viewBox="0 0 100 100" preserveAspectRatio="none">
                         <rect x="0.25" y="0.25" width="99.5" height="99.5" rx="2" ry="2" />
                     </svg>
-                    <h1>İşinize uygun Yapay Zeka Asistanlar</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sint corrupti beatae nemo odio amet quos maiores delectus laboriosam dolore quisquam eaque corporis hic debitis quod, earum adipisci! Odio, sunt?
+                    <h1 className={styles.cardtitle} >İşinize uygun Yapay Zeka Asistanlar</h1>
+                    <p className={styles.cardtext} > Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sint corrupti beatae nemo odio amet quos maiores delectus laboriosam dolore quisquam eaque corporis hic debitis quod, earum adipisci! Odio, sunt?
                     </p>
-                    <Link href="/">Detaylı Bilgi</Link>
+                    <Link className={styles.cardlink} href="/">Detaylı Bilgi</Link>
                 </div>
                 <div className={`${styles.card} ${styles.card2}`}>
                     <svg className={styles.borderAnim} viewBox="0 0 100 100" preserveAspectRatio="none">
                         <rect x="0.25" y="0.25" width="99.5" height="99.5" rx="4" ry="8" />
                     </svg>
-                    <h2  >Yapa Zeka Destekli Mobil Uygulamalar</h2>
-                    <p>Yapa Zeka Destekli Mobil Uygulamalar</p>
-                    <Link href="/">Detaylı Bilgi</Link>
+                    <h1 className={styles.cardtitle} >Web siteleri</h1>
+                    <p className={styles.cardtext} >  consectetur adipisicing elit. Aspernatur sint corru eaque corporis hic debitis quod, earum adipisci! Odio, sunt?
+                    </p>
+                    <Link className={styles.cardlink} href="/">Detaylı Bilgi</Link>
                 </div>
                 <div className={`${styles.card} ${styles.card3}`}>
                     <svg className={styles.borderAnim} viewBox="0 0 100 100" preserveAspectRatio="none">
                         <rect x="0.25" y="0.25" width="99.5" height="99.5" rx="4" ry="8" />
                     </svg>
-                    <h2>Sunucu taraflı uygulamalar</h2>
-                    <p>Sunucu taraflı uygulamalar</p>
-                    <Link href="/">Detaylı Bilgi</Link>
+                    <h1 className={styles.cardtitle} >Mobil Uygulamalar</h1>
+                    <p className={styles.cardtext} >  maiores delectus laboriosam dolore quisquam eaque corporis hic debitis quod, earum adipisci! Odio, sunt?
+                    </p>
+                    <Link className={styles.cardlink} href="/">Detaylı Bilgi</Link>
                 </div>
                 <Link className={styles.moreproductslink} href="/">
-                <Image src={arrowrigth} alt='daha fazla' />
+                    <Image src={arrowrigth} alt='daha fazla' />
                 </Link>
             </div>
         </div>
